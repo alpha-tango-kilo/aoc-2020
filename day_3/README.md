1b2d5eeddec1dfe01b13d52de57d97e213228f65
alpha-tango-kilo <atk@aaathats3as.com>
2020-12-12 21:24:14 +0000

    Day three part two

--- day_3/README.md
+++ day_3/README.md
@@ -5,4 +5,12 @@
 ## Part one
 
 I really enjoyed this challenge.
-I didn't try and rush it as fast as I could or make it as lean as possible, and the nicety afforded by doing things properly with enums and structs makes a big difference for readability.
+I didn't try and rush it as fast as I could or make it as lean as possible, and the nicety afforded by doing things properly with enums and structs makes a big difference for readability
+
+## Part two
+
+I was so happy when I saw this challenge.
+I had made my system of `Vec2D`s anticipating this scenario and actually hitting the nail on the head (unlike the previous two days) felt great and made this solution super quick and clean to write.
+I literally didn't have to touch my `lib.rs`
+
+I also love that Rust iterators have a `.product()` method
--- day_3/src/main.rs
+++ day_3/src/main.rs
@@ -2,33 +2,60 @@ use anyhow::{Result, Context};
 use std::fs;
 use day_3::{Slope, Vec2D, trees_hit};
 
+const PART_TWO_SLOPES: [Vec2D; 5] = [
+    Vec2D { x: 1, y: 1},
+    Vec2D { x: 3, y: 1},
+    Vec2D { x: 5, y: 1},
+    Vec2D { x: 7, y: 1},
+    Vec2D { x: 1, y: 2},
+];
+
 fn main() -> Result<()> {
     let input = fs::read_to_string("./day_3/input")?;
     let slope = Slope::new(&input).context("Failed to parse slope")?;
     let start = Vec2D { x: 0, y: 0 };
     let vector = Vec2D { x: 3, y: 1 };
 
-    let trees_hit = trees_hit(&slope, &start, &vector)
+    let part_one_trees_hit = trees_hit(&slope, &start, &vector)
         .context("Failed to calculate trees hit")?;
 
-    println!("Trees hit: {}", trees_hit);
+    println!("Trees hit: {}", part_one_trees_hit);
+
+    let part_two_product_of_all_trees_hit = PART_TWO_SLOPES.iter()
+        .filter_map(|vector| { trees_hit(&slope, &start, vector).ok() })
+        .product::<usize>();
+
+    println!("Product of all trees hit: {}", part_two_product_of_all_trees_hit);
+
     Ok(())
 }
 
 #[cfg(test)]
 mod tests {
-    use crate::{Slope, Vec2D, trees_hit};
+    use crate::{Slope, Vec2D, trees_hit, PART_TWO_SLOPES};
+
+    const INPUT: &str = "..##.......\n#...#...#..\n.#....#..#.\n..#.#...#.#\n.#...##..#.\n..#.##.....\n.#.#.#....#\n.#........#\n#.##...#...\n#...##....#\n.#..#...#.#";
 
     #[test]
     fn example_data_part_one() {
-        let input = "..##.......\n#...#...#..\n.#....#..#.\n..#.#...#.#\n.#...##..#.\n..#.##.....\n.#.#.#....#\n.#........#\n#.##...#...\n#...##....#\n.#..#...#.#";
-
-        let slope = Slope::new(input).expect("Failed to parse slope");
+        let slope = Slope::new(&INPUT).expect("Failed to parse slope");
         let start = Vec2D { x: 0, y: 0 };
         let vector = Vec2D { x: 3, y: 1 };
 
         let trees_hit = trees_hit(&slope, &start, &vector)
             .expect("Failed to calculate trees hit");
         assert_eq!(trees_hit, 7);
+    }
+
+    #[test]
+    fn example_data_part_two() {
+        let slope = Slope::new(&INPUT).expect("Failed to parse slope");
+        let start = Vec2D { x: 0, y: 0 };
+
+        let part_two_product_of_all_trees_hit = PART_TWO_SLOPES.iter()
+            .filter_map(|vector| { trees_hit(&slope, &start, vector).ok() })
+            .product::<usize>();
+
+        assert_eq!(part_two_product_of_all_trees_hit, 336);
     }
 }
