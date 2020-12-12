0b11c0198f353e4f356c587c364c672fe10e5c35
alpha-tango-kilo <atk@aaathats3as.com>
2020-12-12 15:23:39 +0000

    Day one part two

--- day_1/README.md
+++ day_1/README.md
@@ -5,3 +5,10 @@
 So for this problem I didn't bother with a sophisticated solution, settling for n squared big O and using two nested for loops.
 
 Probably not the most idiomatic Rust solution either given the lack of iterators, but it'll do
+
+## Part two
+
+Well with my part one solution I failed to guess what they'd make harder for part two (changing the number to be summed to), however it did still end up helping.
+
+Changing some function signatures made return types more useful and allowed for reuse of code I had already written.
+Yes my solution is O(n^3) but at the same time I feel like the compiler probably sorted a lot of that out given how fast everything ran
--- day_1/src/main.rs
+++ day_1/src/main.rs
@@ -6,22 +6,39 @@ fn main() -> Result<(), Box<dyn Error>>{
     let input = input.split('\n')
         .into_iter()
         .filter_map(|s| -> Option<u32> { s.parse().ok() })
-        .collect::<Vec<u32>>();
+        .collect::<Vec<u32>>()
+        .into_boxed_slice();
 
-    if let Some(n) = get_prod_of_those_that_sum(&2020, &input) {
-        println!("Answer: {}", n);
+    if let Some((a, b)) = get_two_that_sum(&2020, &input) {
+        println!("Product of two numbers that sum to 2020: {}", a * b);
     } else {
-        println!("No answer found!");
+        println!("No answer found for two numbers that sum to 2020!");
+    }
+
+    if let Some((a, b, c)) = get_three_that_sum(&2020, &input) {
+        println!("Product of three numbers that sum to 2020: {}", a * b * c);
+    } else {
+        println!("No answer found for three numbers that sum to 2020!");
     }
 
     Ok(())
 }
 
-fn get_prod_of_those_that_sum(to: &u32, list: &Vec<u32>) -> Option<u32> {
+fn get_two_that_sum<'a>(to: &u32, list: &'a [u32]) -> Option<(&'a u32, &'a u32)> {
     for (n, a) in list.iter().enumerate() {
         let (_, bs) = list.split_at(n);
         if let Some((a, b)) = check_for_sum(to, a, bs) {
-            return Some(a * b);
+            return Some((a, b));
+        }
+    }
+    None
+}
+
+fn get_three_that_sum<'a>(to: &u32, list: &'a [u32]) -> Option<(&'a u32, &'a u32, &'a u32)> {
+    for (n, a) in list.iter().enumerate() {
+        let (_, bs) = list.split_at(n);
+        if let Some((b, c)) = get_two_that_sum(&(to - a), bs) {
+            return Some((a, b, c));
         }
     }
     None
@@ -38,13 +55,29 @@ fn check_for_sum<'a>(to: &u32, a: &'a u32, bs: &'a [u32]) -> Option<(&'a u32, &'
 
 #[cfg(test)]
 mod tests {
-    use crate::get_prod_of_those_that_sum;
+    use crate::*;
 
     #[test]
-    fn example_data() {
+    fn example_data_part_one() {
+        let input = vec![1721, 979, 366, 299, 675, 1456];
+        let (a, b) = get_two_that_sum(&2020, &input)
+            .expect("No result!");
+
         assert_eq!(
-        get_prod_of_those_that_sum(&2020, &vec![1721, 979, 366, 299, 675, 1456]),
-        Some(514579),
+            a * b,
+            514579,
+        );
+    }
+
+    #[test]
+    fn example_data_part_two() {
+        let input = vec![1721, 979, 366, 299, 675, 1456];
+        let (a, b, c) = get_three_that_sum(&2020, &input)
+            .expect("No result!");
+
+        assert_eq!(
+            a * b * c,
+            241861950,
         );
     }
 }
