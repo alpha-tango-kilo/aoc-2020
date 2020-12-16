974d33c5126ea9890a71c47c5be3afe08c58ded8
alpha-tango-kilo <atk@aaathats3as.com>
2020-12-16 17:56:48 +0000

    Start day nine

---
+++ day_9/README.md
@@ -0,0 +1,7 @@
+# Day Nine - Encoding Error
+
+[**Link**](https://adventofcode.com/2020/day/9)
+
+## Part one
+
+
---
+++ day_9/src/main.rs
@@ -0,0 +1,14 @@
+use anyhow::Result;
+
+fn main() -> Result<()> {
+    println!("Hello, world!");
+    Ok(())
+}
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn example_data_part_one() {
+
+    }
+}
